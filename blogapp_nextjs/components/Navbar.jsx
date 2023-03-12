2dce964d98a7e9a55b8add5b70764067c0dfe374
yuma0313 <y.yuma0313@gmail.com>
2023-03-12 16:03:23 +0900

    navbar、その他ファイル作成

---
+++ blogapp_nextjs/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+const Navbar = () => {
+  return (
+    <div className="bg-gray-800">
+      <div className="h-16 px-8 flex items-center">
+        <p className="text-white font-bold flex-auto ">BlogApp</p>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
